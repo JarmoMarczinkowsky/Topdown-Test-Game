e1fb4fa13dc31fe5b3f2ceb4803e0362977ccb44
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-05-24 11:26:08 +0200

    Added shootable bullets

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public Rigidbody2D rb;
-
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +20,15 @@ public class PlayerMovement : MonoBehaviour
         float dirY = Input.GetAxis("Vertical") * speed;
 
         rb.velocity = new Vector2(dirX, dirY);
+
+        if(Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            transform.Rotate(0, 180, 0);
+        }
+        else if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            transform.Rotate(0, 0, 0);
+        }
+        
     }
 }
