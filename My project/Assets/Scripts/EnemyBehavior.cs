2b99cfe3ee3368cd4789160be1c3b06d5eea9c4f
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-05-28 00:58:38 +0200

    Added color on enemy hit

--- My project/Assets/Scripts/EnemyBehavior.cs
+++ My project/Assets/Scripts/EnemyBehavior.cs
@@ -6,20 +6,36 @@ public class EnemyBehavior : MonoBehaviour
 {
     public float Hitpoints; 
     public float MaxHitPoints = 10f;
+    public float DamageVisibleTime = .1f;
+
+    private SpriteRenderer spriteColor;
 
     // Start is called before the first frame update
     void Start()
     {
         Hitpoints = MaxHitPoints;
+        spriteColor = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
     {
         Hitpoints -= damage;
+        spriteColor.color = Color.red;
+
         if (Hitpoints <= 0)
         {
             Destroy(gameObject);
         }
+
+        StartCoroutine(WaitTime(DamageVisibleTime));
+
+        Debug.Log("Change Color back");
+    }
+
+    IEnumerator WaitTime(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        spriteColor.color = Color.white;
     }
     
 }
