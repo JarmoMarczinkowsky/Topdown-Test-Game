f733e49e1e7373fed25ca8084a7da04aa4e2414b
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-05-28 22:17:17 +0200

    Added ammo score

--- My project/Assets/Scripts/PlayerShooter.cs
+++ My project/Assets/Scripts/PlayerShooter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,22 +10,26 @@ public class PlayerShooter : MonoBehaviour
     public Transform fireballPoint;
     public GameObject bulletPrefab;
     public GameObject fireballPrefab;
+    public TextMeshPro AmmoScore;
     public static int BulletCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        AmmoScore.text = BulletCount.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+        AmmoScore.text = BulletCount.ToString();
+
         if (Keyboard.current.spaceKey.wasPressedThisFrame && BulletCount > 0)
         {
             Debug.Log("Created bullet");
             Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
             BulletCount--;
+            
         }
 
         if(Keyboard.current.fKey.wasPressedThisFrame)
