98b9a8568bd4429e0f8f3fea968ed17c7c2400ab
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-05-30 11:16:20 +0200

    Added input for goblin spawner

--- My project/Assets/Scripts/EnemySpawner.cs
+++ My project/Assets/Scripts/EnemySpawner.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject MyEnemy;
+    public float TimeToWait = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WaitTime(5, MyEnemy));
+        StartCoroutine(WaitTime(TimeToWait, MyEnemy));
     }
 
     // Update is called once per frame
@@ -21,7 +23,15 @@ public class EnemySpawner : MonoBehaviour
     IEnumerator WaitTime(float time, GameObject swarmPrefab)
     {
         yield return new WaitForSeconds(time);
-        Instantiate(swarmPrefab, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0), Quaternion.identity);
-        StartCoroutine(WaitTime(5, swarmPrefab));
+        Instantiate(swarmPrefab, new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0), Quaternion.identity);
+
+        if (Keyboard.current.shiftKey.isPressed)
+        {
+            StartCoroutine(WaitTime(TimeToWait / 0.1f, swarmPrefab));
+        }
+        else
+        {
+            StartCoroutine(WaitTime(TimeToWait, swarmPrefab));
+        }
     }
 }
