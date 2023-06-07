5913cb30759130faa5ae17c3b0f5e9a1abdfabc0
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-06-07 11:57:53 +0200

    Added player movement

---
+++ Spacescroller/Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(0, moveY) * speed;
+
+        if (Keyboard.current.sKey.isPressed) //down
+        {
+            //rotate sprite to look -45 degrees down
+            transform.rotation = Quaternion.Euler(0, 0, -15);
+        }
+        else if (Keyboard.current.wKey.isPressed)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 15);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
