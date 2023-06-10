5c415b8c7299322c2dd7b73a4d06f87ffcc5537d
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-06-10 23:39:49 +0200

    Added enemy with healthbar

--- Spacescroller/Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Spacescroller/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -6,16 +6,20 @@ public class EnemyBehavior : MonoBehaviour
 {
     public float Hitpoints;
     public float MaxHitpoints = 5;
+    public HealthBarBehavior Healthbar;
 
     // Start is called before the first frame update
     void Start()
     {
         Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
     }
 
     public void TakeHit(float damage)
     {
         Hitpoints -= damage;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+
         if (Hitpoints <= 0)
         {
             Destroy(gameObject);
